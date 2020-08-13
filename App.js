4e9433418ec9d2b290b0ef14f75310f0508c2688
pyBae-py <sajeel.ahmed@protonmail.com>
2020-08-13 11:55:26 +0500

    :new: Add Functionality

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [enteredNum, setEnderedNum] = useState(0);
+  let matched = false;
+
+  const checkNum = () => {
+    let randomNum = 9999;
+    if (randomNum == enteredNum) {
+      matched = true;
+      alert("Hurrah!, It matched");
+    } else {
+      matched = false;
+      alert("Alas!, Please try again");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Welcome, Sajeel</Text>
+      <View style={styles.main}>
+        <TextInput
+          style={styles.inputField}
+          placeholder='Enter The Number'
+          keyboardType='number-pad'
+          value={enteredNum}
+          onChangeText={(e) => {
+            setEnderedNum(e);
+          }}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Button title='Guess' onPress={checkNum} />
+        </TouchableOpacity>
+      </View>
+
+      <StatusBar style='dark' />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  heading: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  main: {},
+  inputField: {
+    borderWidth: 0.5,
+    borderRadius: 5,
+    padding: 10,
+    paddingBottom: 13,
+    margin: 10,
+    width: 150,
+  },
+  button: { margin: 10, marginBottom: 0 },
 });
+
+export default App;
